509cc2554c926225e153f4c4a37559a2551f6130
alex <alex.logorz@gmail.com>
2021-08-12 17:46:26 -0400

    test

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
-const mongoose = require('mongoose')
 const path = require('path')
 
 app.use(cors());
