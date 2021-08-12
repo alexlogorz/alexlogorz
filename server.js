1f6deabe1c2523382fa8e14eb08f65905a3801cb
alex <alex.logorz@gmail.com>
2021-08-12 17:40:28 -0400

    test

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
-const express = require('express')
-const app = express()
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+const mongoose = require('mongoose')
 const path = require('path')
 
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'))
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    })
-} 
+app.use(cors());
+app.use(express.json());
 
-app.listen(PORT, () => console.log(`listening on port ${PORT}`))
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static('client/build'))
+    app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
+app.listen(port, () => console.log(`listening on port ${port}`));
